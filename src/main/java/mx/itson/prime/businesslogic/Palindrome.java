e4e0ff531f71630945e47b1bf942e7195e49e211
alumnog <alumnog@S921-13.itson.edu.mx>
2025-08-28 11:27:25 -0700

    ciclo para determinar si es un palindromo(checar)

--- src/main/java/mx/itson/prime/businesslogic/Palindrome.java
+++ src/main/java/mx/itson/prime/businesslogic/Palindrome.java
@@ -11,6 +11,31 @@ package mx.itson.prime.businesslogic;
 public class Palindrome {
     
     public static boolean isPalindrome(String value){
+       // le quita e´pacios y lo pone en minusculas 
+        String text= value.replaceAll("\\s", "").toLowerCase();
+        
+        char[] letras = text.toCharArray();
+        
+        int count = letras.length;
+        
+        
+       for (char p: letras){
+           
+           if(p != letras [count -1]){
+           return false;
+          
+           }else{
+           count -=1;
+            if (count <0){
+
+            return true;}
+           
+           }
+    
+    }
         return true;
+        
+        
+    
     }
 }
